ff30040b07c6b35a712ba7fbbf87fdf4214469d3
Ishmam Sultan <106390341@student.swin.edu.au>
2026-05-06 03:32:23 +1000

    code structured

--- daysarray.php
+++ daysarray.php
@@ -6,8 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo "<h1>PHP Variables, Arrays and Operators</h1>";
-    ?>
+<?php
+    echo "<h1>PHP Variables, Arrays and Operators</h1>";
+    $days_en = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"); 
+    echo "<p>The days of the week in English are:<br>";
+    echo $days_en[0] . ", " . 
+         $days_en[1] . ", " . 
+         $days_en[2] . ", " . 
+         $days_en[3] . ", " . 
+         $days_en[4] . ", " . 
+         $days_en[5] . ", " . 
+         $days_en[6] . ".</p>";
+
+    $days_fr = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+    
+    echo "<p>The days of the week in French are:<br>";
+    echo $days_fr[0] . ", " . 
+         $days_fr[1] . ", " . 
+         $days_fr[2] . ", " . 
+         $days_fr[3] . ", " . 
+         $days_fr[4] . ", " . 
+         $days_fr[5] . ", " . 
+         $days_fr[6] . ".</p>";
+?>
 </body>
 </html>
